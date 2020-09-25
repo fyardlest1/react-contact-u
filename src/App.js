f58fb34ddd37e2879e71b35a5fa339c3b03c4df8
Fyardlest <fyardlest1@gmail.com>
2020-09-25 08:22:01 -0400

    Dynamically Render Pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
+import * as ContactsAPI from './utils/ContactsAPI';
+import CreateContact from './CreateContact';
 
 class App extends Component {
   state = {
-    contacts: [
-        {
-          id: "karen",
-          name: "Karen Isgrigg",
-          handle: "@karen_isgrigg",
-          avatarURL: "http://localhost:5001/karen.jpg",
-        },
-        {
-          id: "richard",
-          name: "Richard Kalehoff",
-          handle: "@richardkalehoff",
-          avatarURL: "http://localhost:5001/richard.jpg",
-        },
-        {
-          id: "tyler",
-          name: "Tyler McGinnis",
-          handle: "@tylermcginnis",
-          avatarURL: "http://localhost:5001/tyler.jpg",
-        },
-    ]
+    contacts: [],
+    screen: 'list'
+  }
+
+  // Lifecycle method
+  componentDidMount() {
+    ContactsAPI.getAll()
+    .then((contacts) => {
+      this.setState(() => ({
+        contacts
+      }))
+    })
   }
 
   removeContact = (contact) => {
@@ -30,16 +24,24 @@ class App extends Component {
       contacts: currentState.contacts.filter(c => {
         return c.id !== contact.id;
       })
-    }))
+    }));
+
+    ContactsAPI.remove(contact);
   }
 
   render() {
     return (
       <div>
-        <ListContacts 
+        {this.state.screen === 'list' && <ListContacts 
           contacts={this.state.contacts}
           onDeleteContact={this.removeContact}
-        />
+          onNavigate={() => {
+            this.setState(() => ({
+              screen: 'create'
+            }))
+          }}
+        />}
+        {this.state.screen === "create" && <CreateContact />}
       </div>
     );
   }
